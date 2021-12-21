6c83368bba654f630ae8d1cf5856fd83f40b5e5a
Jeryn <56923643+Cadaei-Yuvxvs@users.noreply.github.com>
2021-12-21 18:36:57 +1000

    Instantiating OSHy_data

--- test/OSHy-X_test.py
+++ test/OSHy-X_test.py
@@ -1,7 +1,8 @@
 import pytest
 import pytest_mock
-from OSHyX import convert_to_bool
+from OSHyX import *
 
+# Test convert_to_bool
 @pytest.mark.parametrize("boolstrings", [
     "true",
     "True",
@@ -55,6 +56,30 @@ def test_convert_to_bool(boolstrings):
         convert_to_bool(boolstrings)
     assert str(error_info.value) == 'Input must be a string.'
 
-if __name__ == "__main__":
+# Mock functions
 
-    pass
+mocker.patch(
+    'OSHyX.ants.image_read',
+    return_value=ants.make_image((100,100,100))
+)
+
+mocker.patch(
+    'OSHyX.glob.glob',
+    return_value=["image.nii.gz" for i in range(10)]
+)
+
+# Create an instance of OSHy_data. Methods are never called outside of the class.
+oshy_dat = OSHy_data(tesla = '3', 
+                    weighting = 'T1w',
+                    bimodal = True,
+                    crop = True)
+# Create an instance of Target_img
+
+# my_image = Target_img(img_file = 'sub-XX_T1w.nii.gz', 
+#                       crop = True,
+#                       weighting = 'T1w',
+#                       denoise = True, 
+#                       b1_bias = True,
+#                       out_dir = 'out_dir',
+#                       oshy_data = oshy_dat
+#                       )
