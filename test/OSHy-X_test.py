1bbbc058cde25e67a9550a4d9193f2a3f75ab792
Jeryn <56923643+Cadaei-Yuvxvs@users.noreply.github.com>
2021-12-21 20:39:00 +1000

    test run_JLF2

--- test/OSHy-X_test.py
+++ test/OSHy-X_test.py
@@ -1,5 +1,5 @@
 import pytest
-import pytest_mock
+from pytest_mock import mocker
 from OSHyX import *
 
 # Test convert_to_bool
@@ -56,30 +56,90 @@ def test_convert_to_bool(boolstrings):
         convert_to_bool(boolstrings)
     assert str(error_info.value) == 'Input must be a string.'
 
-# Mock functions
+# Create an instance of OSHy_data. Methods are never called outside of this class.
+oshy_dat = test_OSHy_data(mocker):
+    mocker.patch(
+        'OSHyX.ants.image_read',
+        return_value="An ANTsImage object"
+    )
 
-mocker.patch(
-    'OSHyX.ants.image_read',
-    return_value=ants.make_image((100,100,100))
-)
+    mocker.patch(
+        'OSHyX.glob.glob',
+        return_value=["image.nii.gz" for i in range(10)]
+    )
 
-mocker.patch(
-    'OSHyX.glob.glob',
-    return_value=["image.nii.gz" for i in range(10)]
-)
+    oshy_dat = OSHy_data(tesla = '3', 
+                        weighting = 'T1w',
+                        bimodal = True,
+                        crop = True)
+    
+    return(oshy_dat)
 
-# Create an instance of OSHy_data. Methods are never called outside of the class.
-oshy_dat = OSHy_data(tesla = '3', 
-                    weighting = 'T1w',
-                    bimodal = True,
-                    crop = True)
-# Create an instance of Target_img
+# my_image.run_JLF2(nprocs=args['nthreads'])
 
-# my_image = Target_img(img_file = 'sub-XX_T1w.nii.gz', 
-#                       crop = True,
-#                       weighting = 'T1w',
-#                       denoise = True, 
-#                       b1_bias = True,
-#                       out_dir = 'out_dir',
-#                       oshy_data = oshy_dat
-#                       )
+# if convert_to_bool(args['mosaic']):
+#     my_image.create_mosaic()
+
+# my_image.calc_volume()
+# my_image.resample_segmentation()
+# my_image.threshold_structures()
+
+
+
+def test_run_JLF2(mocker):
+    mocker.patch('OSHyX.os.path.exists', return_value=True)
+    mocker.patch('OSHyX.ants.image_write', return_value=None)
+    mocker.patch('OSHyX.ants.image_read', return_value="An ANTsImage object")
+    mocker.patch('OSHyX.ants.denoise_image', return_value="An ANTsImage object")
+    mocker.patch('OSHyX.subprocess.Popen', return_value="A Popen object")
+    mocker.patch('OSHyX.subprocess.Popen.wait', return_value=None)
+    mocker.patch('OSHyX.subprocess.Popen.communicate', return_value=('Info', 'Errors'))
+    mocker.patch('OSHyX.os.remove', return_value=None)
+    mocker.patch('OSHyX.ants.registration', return_value={})
+    mocker.patch('OSHyX.ants.apply_transforms', return_value="An ANTsImage object")
+    mocker.patch('OSHyX.ants.crop_image', return_value="An ANTsImage object")
+    my_image = Target_img(img_file = 'sub-XX_T1w.nii.gz', 
+                        crop = True,
+                        weighting = 'T1w',
+                        denoise = True, 
+                        b1_bias = True,
+                        out_dir = 'out_dir',
+                        oshy_data = oshy_dat
+                        )
+
+    spy = mocker.spy(subprocess, 'Popen')
+    assert my_image.run_JLF2(2) == None
+
+    spy.assert_called_once_with(
+        [
+            "antsJointLabelFusion2.sh", 
+            "-d", "3", "-j", 2,
+            "-o", "out_dir/sub-XX_",
+            "-t", "out_dir/sub-XX_denoised_bias-corrected_cropped_T1w.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-g", "image.nii.gz", "-l", "image.nii.gz",
+            "-b" ,"1", "-b", "2", "-b" "3", "-b", "4", "-a", "4", "-s", "10"], 
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True
+
+    )
